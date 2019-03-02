8c94911387c4a51553d057e570759c17c4c9a5f6
vasylmeister <vasylmeister@gmail.com>
2019-03-02 01:59:25 -0500

    delete file

--- js/mama.js
+++
@@ -1,9 +0,0 @@
-	let a = document.querySelectorAll('[data-validation]');
-	function sample(obj) {
-		let b = []
-		for (let el of obj) {
-			b.push(el["id"]);
-		}
-		console.log(b)
-	};
-	sample(a);alert("Here");
